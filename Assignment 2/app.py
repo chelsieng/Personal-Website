c1b2ba7388adbfa897749e617858b299bd7265d1
chelsieng <chelsiengmk@gmail.com>
2020-02-27 15:40:41 -0500

    project page created

--- Assignment 2/app.py
+++ Assignment 2/app.py
@@ -14,5 +14,10 @@ def contact():
     return render_template("contact.html")
 
 
+@app.route('/project')
+def project():
+    return render_template("project.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
