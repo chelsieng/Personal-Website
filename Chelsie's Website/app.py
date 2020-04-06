81c88d53dec4ef403544d49ad214a533f31164ca
chelsieng <chelsiengmk@gmail.com>
2020-04-06 00:16:40 -0400

    Moved database to data directory

--- Chelsie's Website/app.py
+++ Chelsie's Website/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contact.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/contact.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
